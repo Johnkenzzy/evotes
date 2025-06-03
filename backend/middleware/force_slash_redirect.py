54f593fcc098b05c6c5b3dee8befdc9df47bc520
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-03 05:53:04 +0100

    307-For POST and PUTT Requests Trailing-slash

--- backend/middleware/force_slash_redirect.py
+++ backend/middleware/force_slash_redirect.py
@@ -1,3 +1,4 @@
+import re
 from django.http import HttpResponsePermanentRedirect
 from django.urls import resolve, Resolver404
 from urllib.parse import urlsplit, urlunsplit
@@ -15,25 +16,26 @@ class ForceSlashMiddleware:
     def __call__(self, request):
         path = request.path_info
 
-        if not path.endswith('/'):
-            try:
-                # Try to resolve the slash-appended path
-                resolve(path + '/')
-            except Resolver404:
-                # No match with slash, don't redirect
-                return self.get_response(request)
+        if request.method == 'POST' or request.method == 'PUT':
+            if not path.endswith('/'):
+                try:
+                    # Try to resolve the slash-appended path
+                    resolve(path + '/')
+                except Resolver404:
+                    # No match with slash, don't redirect
+                    return self.get_response(request)
 
-            # Rebuild full URL with trailing slash
-            split = urlsplit(request.get_full_path())
-            new_path = urlunsplit(
-                (split.scheme,
-                 split.netloc,
-                 split.path + '/',
-                 split.query,
-                 split.fragment)
-                )
+                # Rebuild full URL with trailing slash
+                split = urlsplit(request.get_full_path())
+                new_path = urlunsplit(
+                    (split.scheme,
+                    split.netloc,
+                    split.path + '/',
+                    split.query,
+                    split.fragment)
+                    )
 
-            # Use 307 to preserve method (POST, PUT, etc.)
-            return HttpResponsePermanentRedirect(new_path, status=307)
+                # Use 307 to preserve method (POST and PUT)
+                return HttpResponsePermanentRedirect(new_path, status=307)
 
         return self.get_response(request)
